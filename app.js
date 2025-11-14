15cd5b0dd89540e1288ce586b4ecebd69fe82b0e
Kmoney <kmoneyfreefire@gmail.com>
2025-11-14 15:17:28 +0100

    deployment

---
+++ app.js
@@ -0,0 +1,7 @@
+const hamIcon = document.querySelector(".ham_icon");
+const navLinks = document.querySelector(".navLinks");
+
+hamIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+})
+.about - section;
